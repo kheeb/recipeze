f8ce24c0229b08275e5d364be2a7b7c02173e26c
Rayman244 <rayhewitt42@icloud.com>
2022-02-24 13:26:30 -0500

    blank document

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="flex-column justify-center align-center min-100-vh bg-primary">
+        
+      </div>
+    </Router>
+  );
+}
+
+export default App;
