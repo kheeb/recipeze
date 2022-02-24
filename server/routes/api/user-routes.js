a75c9d5ec56e4eb148895d8d419a68aa2f4a00f7
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-24 14:15:23 -0500

    Added auth middleware function

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -1,4 +1,6 @@
 const router = require('express').Router;
 const { getSingleUser, createUser, login } = require('../../controllers/user-controller');
 
+const { authMiddleware } = require('../../utils/auth');
+
 module.exports = router;
