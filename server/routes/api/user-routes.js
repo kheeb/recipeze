5a92ac3477235477442645c3cd4a0736a070eea0
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-24 14:23:14 -0500

    Added login route

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -6,4 +6,6 @@ const { authMiddleware } = require('../../utils/auth');
 // TODO: Add controller for adding recipe to put
 router.route('/').post(createUser).put(authMiddleware);
 
+router.route('/login').post(login);
+
 module.exports = router;
