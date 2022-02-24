c39a1b05ac66355614e62c1838f57f4fe831febc
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-24 14:08:57 -0500

    Added login

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -21,5 +21,18 @@ module.exports = {
     const token = signToken(user);
     res.json({ token, user });
   },
+  async login({ body }, res) {
+    const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+    if (!user) {
+      return res.status(400).json({ message: "Can't find this user" });
+    }
 
+    const correctPw = await user.isCorrectPassword(body.password);
+
+    if (!correctPw) {
+      return res.status(400).json({ message: 'Wrong password!' });
+    }
+    const token = signToken(user);
+    res.json({ token, user });
+  },
 };
