9b55fbd646698cbed2e07655853412c453424aaa
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-25 13:41:43 -0500

    Added apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -9,14 +9,39 @@ import { setContext } from '@apollo/client/link/context';
 // import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-      <div className="flex-column justify-center align-center min-100-vh bg-primary">
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-center align-center min-100-vh bg-primary">
 
-      </div>
-    </Router>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
