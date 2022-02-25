d55e0c63e516fa4346f753b5899a35e91d85f185
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-25 13:39:57 -0500

    Imported apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 // import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -7,7 +14,7 @@ function App() {
   return (
     <Router>
       <div className="flex-column justify-center align-center min-100-vh bg-primary">
-        
+
       </div>
     </Router>
   );
