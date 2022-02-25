4e3e48b2d641a28ec271d54d1fedc097c1a24eab
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-25 15:38:54 -0500

    Added button to save the recipe to user

--- client/src/components/RecipeCards/index.js
+++ client/src/components/RecipeCards/index.js
@@ -7,7 +7,8 @@ export default function recipeCard({ recipeName, photo, altDesc }) {
       <Card.Title>{recipeName}</Card.Title>
       <Card.Img src={photo} alt={altDesc} />
       <Card.Body>
-
+        {/* // TODO: Change href ternary with variable that shows if the recipe is already saved to the user */}
+        <Button href={('Variable for if saved to user') ? "HREF for saving to user's schedule" : 'HREF for saving to user'}>Save</Button>
       </Card.Body>
     </Card>
   )
