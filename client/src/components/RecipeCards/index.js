e5ba1208b4d53f8a8158be74348f8a950f18de9e
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-25 15:48:37 -0500

    Added options

--- client/src/components/RecipeCards/index.js
+++ client/src/components/RecipeCards/index.js
@@ -11,9 +11,16 @@ export default function recipeCard({ recipeName, photo, altDesc }) {
         <Button href={('Variable for if saved to user') ? "HREF for saving to user's schedule" : 'HREF for saving to user'}>Save</Button>
       </Card.Body>
       {/* // TODO: Add bootstrap styling or custom styling into ternary to show button if saved to user profile */}
-      <Form style={('Variable for is saved to user') ? 'display: whichever display style we chose;' : 'display: hidden;'}>
-
-      </Form>
+      <Form.Select style={('Variable for is saved to user') ? 'display: whichever display style we chose;' : 'display: hidden;'}>
+        <option>Day of the week</option>
+        <option value={'Sunday'}>Sunday</option>
+        <option value={'Monday'}>Monday</option>
+        <option value={'Tuesday'}>Tuesday</option>
+        <option value={'Wednesday'}>Wednesday</option>
+        <option value={'Thursday'}>Thursday</option>
+        <option value={'Friday'}>Friday</option>
+        <option value={'Saturday'}>Saturday</option>
+      </Form.Select>
     </Card>
   )
 }
