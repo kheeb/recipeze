16501bca3acd988da4d7eea72186b99cce0a8774
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-28 10:42:06 -0500

    Commented out code that will not be used, kept just in case

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+// const router = require('express').Router();
+// const apiRoutes = require('./api');
 
-router.use('/api', apiRoutes);
+// router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/index.html'));
-});
+// router.use((req, res) => {
+//   res.sendFile(path.join(__dirname, '../../client/index.html'));
+// });
 
-module.exports = router;
+// module.exports = router;
