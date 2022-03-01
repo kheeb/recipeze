b66d622fae589f4ed89fc8d6a478da8c491ce6f9
Andrew Yoo <andrewbyoo@gmail.com>
2022-03-01 18:04:34 -0500

    Removed unused code and ran code through formatter

--- client/src/components/RecipeCards/index.js
+++ client/src/components/RecipeCards/index.js
@@ -1,17 +1,16 @@
 //Recipe Cards
-import { Card, Button} from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 
 export default function recipeCard({ recipeId, recipeName, recipeLink, photoLink, loggedIn, recipeSaved, handleSave }) {
 
   return (
     <Card data-recipeid={recipeId}>
       <Card.Title>{recipeName}</Card.Title>
-      <Card.Img src={photoLink} alt={`${recipeName} photo`}/>
+      <Card.Img src={photoLink} alt={`${recipeName} photo`} />
       <Card.Body>
-        <Button 
-        onClick={()=>{handleSave(recipeId,recipeName,photoLink,recipeLink )}}
-        // href={(!recipeSaved) ? 'HREF for saving to user' : 'HREF for updating existing saved recipe'}
-         className={(!loggedIn) ? 'invisible' : 'visible'}>Save</Button>
+        <Button
+          onClick={() => { handleSave(recipeId, recipeName, photoLink, recipeLink) }}
+          className={(!loggedIn) ? 'invisible' : 'visible'}>Save</Button>
         <Button href={recipeLink} target='_blank'>Recipe Link</Button>
       </Card.Body>
     </Card>
