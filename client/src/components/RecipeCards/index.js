d64aa8eb7f2d006ac2c2b641690a50bd7d9eb26c
Andrew Yoo <andrewbyoo@gmail.com>
2022-03-01 13:24:23 -0500

    Commented out portion that can't be used, added recipe link, added button for recipe

--- client/src/components/RecipeCards/index.js
+++ client/src/components/RecipeCards/index.js
@@ -1,15 +1,16 @@
 //Recipe Cards
 import { Card, Button, Form } from 'react-bootstrap';
 
-export default function recipeCard({ recipeName, photoLink, loggedIn, recipeSaved }) {
+export default function recipeCard({ recipeId, recipeName, recipeLink, photoLink, loggedIn, recipeSaved }) {
   return (
-    <Card>
+    <Card data-recipeid={recipeId}>
       <Card.Title>{recipeName}</Card.Title>
-      <Card.Img src={photoLink} alt={`${recipeName} photo`} />
+      <Card.Img src={photoLink} alt={`${recipeName} photo`}/>
       <Card.Body>
         <Button href={(!recipeSaved) ? 'HREF for saving to user' : 'HREF for updating existing saved recipe'} className={(!loggedIn) ? 'invisible' : 'visible'}>Save</Button>
+        <Button href={recipeLink} target='_blank'>Recipe Link</Button>
       </Card.Body>
-      <Form.Select className={(!loggedIn) ? 'invisible' : 'visible'}>
+      {/* <Form.Select className={(!loggedIn) ? 'invisible' : 'visible'}>
         <option>Day of the week</option>
         <option value={'Sunday'}>Sunday</option>
         <option value={'Monday'}>Monday</option>
@@ -18,7 +19,7 @@ export default function recipeCard({ recipeName, photoLink, loggedIn, recipeSave
         <option value={'Thursday'}>Thursday</option>
         <option value={'Friday'}>Friday</option>
         <option value={'Saturday'}>Saturday</option>
-      </Form.Select>
+      </Form.Select> */}
     </Card>
   )
 }
