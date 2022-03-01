1efc3a98aeb2ff525d67175125dbf829316bcae3
Andrew Yoo <andrewbyoo@gmail.com>
2022-03-01 13:49:13 -0500

    Added ternary for hiding save button if not logged in

--- client/src/components/RecipeCards/index.js
+++ client/src/components/RecipeCards/index.js
@@ -7,7 +7,7 @@ export default function recipeCard({ recipeId, recipeName, recipeLink, photoLink
       <Card.Title>{recipeName}</Card.Title>
       <Card.Img src={photoLink} alt={`${recipeName} photo`}/>
       <Card.Body>
-        <Button href={(!recipeSaved) ? 'HREF for saving to user' : 'HREF for updating existing saved recipe'} className={(!loggedIn) ? 'invisible' : 'visible'}>Save</Button>
+        <Button href={(!recipeSaved) ? 'HREF for saving to user' : 'HREF for updating existing saved recipe'} className={(!loggedIn) ? 'd-none' : ''}>Save</Button>
         <Button href={recipeLink} target='_blank'>Recipe Link</Button>
       </Card.Body>
       {/* <Form.Select className={(!loggedIn) ? 'invisible' : 'visible'}>
