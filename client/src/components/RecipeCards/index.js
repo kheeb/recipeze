205d9bcc0e59d266b978dacf628047e202ae7870
Andrew Yoo <andrewbyoo@gmail.com>
2022-03-01 14:58:37 -0500

    Updated on click for save button

--- client/src/components/RecipeCards/index.js
+++ client/src/components/RecipeCards/index.js
@@ -7,7 +7,7 @@ export default function recipeCard({ recipeId, recipeName, recipeLink, photoLink
       <Card.Title>{recipeName}</Card.Title>
       <Card.Img src={photoLink} alt={`${recipeName} photo`}/>
       <Card.Body>
-        <Button onClick={handleSave} className={(!loggedIn) ? 'd-none' : ''}>Save</Button>
+        <Button onClick={() => handleSave(recipeId, recipeName, photoLink, recipeLink)} className={(!loggedIn) ? 'd-none' : ''}>Save</Button>
         <Button href={recipeLink} target='_blank'>Recipe Link</Button>
       </Card.Body>
       {/* <Form.Select className={(!loggedIn) ? 'invisible' : 'visible'}>
