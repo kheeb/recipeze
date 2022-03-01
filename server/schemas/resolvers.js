2cab24f8358c21113765ac3ab93144035122ef8d
Andrew Yoo <andrewbyoo@gmail.com>
2022-03-01 18:43:15 -0500

    Removed console logs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -42,7 +42,6 @@ const resolvers = {
       return { token, user };
     },
     saveRecipe: async (parent, { recipe }, context) => {
-      console.log(recipe);
       try {
         const updatedUser = await User.findOneAndUpdate(
           { _id: context.user._id },
@@ -50,14 +49,13 @@ const resolvers = {
           { $push: { savedRecipes: recipe } },
           { new: true, runValidators: true }
         );
-        console.log(updatedUser);
+
         return updatedUser;
       } catch (err) {
         console.log(err);
       }
     },
     removeRecipe: async (parent, { recipeId }, context) => {
-      console.log(context.user)
       if(context.user) {
         const updatedUser = await User.findOneAndUpdate(
             { _id: context.user._id },
