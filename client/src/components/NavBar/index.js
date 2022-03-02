250e31e0f368b3f00d3e59ab602aab3443cae566
Warren Haskins <warrenhaskins1@gmail.com>
2022-03-01 19:56:49 -0500

    updated styling

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,16 +1,10 @@
 // NavBar Component
 //Navbar
 import React, { useState } from "react";
-import {
-  Navbar,
-  Nav,
-  Container,
-  Modal,
-  Tab,
-} from "react-bootstrap";
-import { Link } from 'react-router-dom';
-import SignUp from '../SignUp'
-import LogIn from '../Login'
+import { Navbar, Nav, Container, Modal, Tab } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import SignUp from "../SignUp";
+import LogIn from "../Login";
 
 import Auth from "../../utils/auth";
 
@@ -18,76 +12,87 @@ function NavBar() {
   const [showModal, setShowModal] = useState(false);
 
   return (
-    <div className="NavBar">
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Container fluid>
-          <Navbar.Brand href="/">
-            <img
-              alt=""
-              src="/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{" "}
-            ReciPeZe
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              <Nav.Link href="/">Home</Nav.Link>
-              {Auth.loggedIn() ? (
-                <>
-                  <Nav.Link as={Link} to="/saved">
-                    See Your Recipes
+    <div className="NavBar mb-3">
+    <>
+      <style type="text/css">
+    {`
+    .navbar-custom {
+      background: linear-gradient(to left, #cee4ce, #f1d261);
+      border-bottom: 5px solid #95a687;
+    }
+
+    `}
+  </style>
+        <Navbar variant="custom" expand="lg" className="">
+          <Container fluid>
+            <Navbar.Brand href="/">
+              <img
+                alt=""
+                src="/logo.svg"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />{" "}
+              
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <Nav.Link href="/">Home</Nav.Link>
+                {Auth.loggedIn() ? (
+                  <>
+                    <Nav.Link as={Link} to="/saved">
+                      See Your Recipes
+                    </Nav.Link>
+                    <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
+                  </>
+                ) : (
+                  <Nav.Link onClick={() => setShowModal(true)}>
+                    Login/Sign Up
                   </Nav.Link>
-                  <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
-                </>
-              ) : (
-                <Nav.Link onClick={() => setShowModal(true)}>
-                  Login/Sign Up
-                </Nav.Link>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      {/* set modal data up */}
-      <Modal
-        size="lg"
-        show={showModal}
-        onHide={() => setShowModal(false)}
-        aria-labelledby="signup-modal"
-      >
-               {/* tab container to do either signup or login component */}
-        <Tab.Container defaultActiveKey="login">
-          <Modal.Header closeButton>
-            <Modal.Title id="signup-modal">
-              <Nav variant="pills">
-              <Nav.Item>
-                  <Nav.Link eventKey='login'>Login</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
-                </Nav.Item>
+                )}
               </Nav>
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Tab.Content>
-              <Tab.Pane eventKey="login">
-                <LogIn handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-              <Tab.Pane eventKey="signup">
-                <SignUp handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-            </Tab.Content>
-          </Modal.Body>
-        </Tab.Container>
-      </Modal>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        {/* set modal data up */}
+        <Modal
+          size="lg"
+          show={showModal}
+          onHide={() => setShowModal(false)}
+          aria-labelledby="signup-modal"
+        >
+          {/* tab container to do either signup or login component */}
+          <Tab.Container defaultActiveKey="login">
+            <Modal.Header closeButton>
+              <Modal.Title id="signup-modal">
+                <Nav variant="pills">
+                  <Nav.Item>
+                    <Nav.Link eventKey="login">Login</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="signup">Sign Up</Nav.Link>
+                  </Nav.Item>
+                </Nav>
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Tab.Content>
+                <Tab.Pane eventKey="login">
+                  <LogIn handleModalClose={() => setShowModal(false)} />
+                </Tab.Pane>
+                <Tab.Pane eventKey="signup">
+                  <SignUp handleModalClose={() => setShowModal(false)} />
+                </Tab.Pane>
+              </Tab.Content>
+            </Modal.Body>
+          </Tab.Container>
+        </Modal>
+      </>
     </div>
   );
 }
--- client/src/components/RecipeCards/index.js
+++ client/src/components/RecipeCards/index.js
@@ -4,15 +4,38 @@ import { Card, Button } from 'react-bootstrap';
 export default function recipeCard({ recipeId, recipeName, recipeLink, photoLink, loggedIn, recipeSaved, handleSave }) {
 
   return (
-    <Card data-recipeid={recipeId}>
-      <Card.Title>{recipeName}</Card.Title>
-      <Card.Img src={photoLink} alt={`${recipeName} photo`} />
+    <>
+  <style type="text/css">
+    {`
+    .btn-flat {
+      background-color: #598787;
+      
+    }
+
+    .btn-xxl {
+      padding: 1rem 1.5rem;
+      font-size: 1.5rem;
+      border-radius: 50px;
+      color: #f1d261;
+      border-color: #f1d261; 
+      border-width: 5px;
+    }
+    `}
+  </style>
+<div class="mt-3 text-center">
+    <Card data-recipeid={recipeId} style={{width: '25rem'}} className="shadow">
+      <Card.Img varient="top" src={photoLink} alt={`${recipeName} photo`} />
+      <Card.Title >{recipeName}</Card.Title>
+      <hr className="hr-custom"></hr>
       <Card.Body>
         <Button
           onClick={() => { handleSave(recipeId, recipeName, photoLink, recipeLink) }}
-          className={(!loggedIn) ? 'invisible' : 'visible'}>Save</Button>
-        <Button href={recipeLink} target='_blank'>Recipe Link</Button>
+          className={(!loggedIn) ? 'invisible' : 'visible'} className="flat" size="xxl" class="">Save</Button>
+        <Button href={recipeLink} target='_blank' className="flat" size="xxl" class="">Link</Button>
       </Card.Body>
     </Card>
+    </div>
+    </>
+    
   )
 }
